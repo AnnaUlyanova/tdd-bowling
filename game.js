67a17182570c5ce90ad344cdc8a07f9919289ba0
amanda-alice <jsmare@gmail.com>
2016-12-01 15:18:22 +1300

    add doubleStrike function

--- game.js
+++ game.js
@@ -1,7 +1,33 @@
 module.exports = {
-  scoreFrame: scoreFrame
+  scoreFrame: scoreFrame,
+  spareFrame: spareFrame,
+  strikeFrame: strikeFrame,
+  doubleStrikeFrame: doubleStrikeFrame,
+  normalScoreFrame: normalScoreFrame
 }
 
-function scoreFrame (frame) {
-  return frame[0] + frame[1];
+var frame1 = [10, 0]
+var frame2 = [10, 0]
+var frame3 = [4, 3]
+var frame4 = [5, 5]
+var frame5 = [4, 2]
+
+function scoreFrame (frame1) {
+  return frame1[0] + frame1[1];
+}
+
+function normalScoreFrame (frame3) {
+  return frame3[0] + frame3[1];
+}
+
+function spareFrame (frame4, frame5) {
+  return 10 + frame5[0];
+}
+
+function strikeFrame (frame1, frame2) {
+  return 10 + frame2[0] + frame2[1];
+}
+
+function doubleStrikeFrame (frame1, frame2, frame3) {
+return 20 + frame3[0] + frame3[1];
 }
--- tests/index.js
+++ tests/index.js
@@ -2,18 +2,47 @@ var test = require('tape')
 var game = require('../game')
 
 test('scores a gutterball frame', function (t) {
-  var frame = [0, 0]
-  var expected = 0
-  var actual = game.scoreFrame(frame)
+  var frame1 = [10, 0]
+  var expected = 10
+  var actual = game.scoreFrame(frame1)
   t.equal(actual, expected)
   t.end()
 })
 
 
 test('scores a normal frame', function (t) {
-  var frame = [2, 3]
-  var expected = 5
-  var actual = game.scoreFrame(frame)
+  var frame3 = [4, 3]
+  var expected = 7
+  var actual = game.normalScoreFrame(frame3)
+  t.equal(actual, expected)
+  t.end()
+})
+
+test('scores a spare frame', function (t) {
+  var frame4 = [5, 5]
+  var frame5 = [4, 2]
+  var expected = 14
+  var actual = game.spareFrame(frame4, frame5)
+  t.equal(actual, expected)
+  t.end()
+
+})
+
+test('scores a single strike frame', function (t) {
+  var frame1 = [10, 0]
+  var frame2 = [4, 3]
+  var expected = 17
+  var actual = game.strikeFrame(frame1, frame2)
+  t.equal(actual, expected)
+  t.end()
+})
+
+test('scores a double strike frame', function (t) {
+  var frame1 = [10, 0]
+  var frame2 = [10, 0]
+  var frame3 = [4, 3]
+  var expected = 27
+  var actual = game.doubleStrikeFrame(frame1, frame2, frame3)
   t.equal(actual, expected)
   t.end()
 })
